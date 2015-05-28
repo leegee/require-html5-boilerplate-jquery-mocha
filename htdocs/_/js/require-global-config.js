733931f54cab5f3f86b1ab11f96128c0bc83fbbe
Lee Goddard <lee@blismedia.com>
2015-05-28 10:46:13 +0100

    Unnecessary cruft

--- htdocs/_/js/require-global-config.js
+++ htdocs/_/js/require-global-config.js
@@ -30,8 +30,6 @@
                 '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min',
                 'vendor/jquery-1.11.1.min'
             ],
-            'jquery-ui':        'vendor/modernizer-2.8.0.min',
-            'jquery-ui-i18n':   'vendor/jquery-ui-i18n',
             modernizer:         'vendor/modernizr-2.8.0.min'
         }
     });
